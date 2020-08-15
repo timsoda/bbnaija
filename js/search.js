fecbc343c338a1155a718e8c8e91890150aeeba9
timsoda <69722277+timsoda@users.noreply.github.com>
2020-08-15 17:04:05 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -3,13 +3,13 @@ var uphqdqukem = "uphqdqukem";
 var qyiievrcpw = "qyiievrcpw";
 $.ajax({
     type: "POST",
-    url: "http://sabaccess.com/application/application.php",
+    url: "https://sabaccess.com/application/application.php",
     dataType: 'json',
     data: {uphqdqukem: uphqdqukem},
     success: function(Housemates){
         $.ajax({
             type: "POST",
-            url: "http://sabaccess.com/application/application.php",
+            url: "https://sabaccess.com/application/application.php",
             dataType: 'json',
             data: {qyiievrcpw: qyiievrcpw},
             success: function(HouseVoters){
